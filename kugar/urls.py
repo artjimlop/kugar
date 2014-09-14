1ce9ca0eb33149e819d1f9b2678f67a9171ca742
Arturo Jimenez <artjimlop@gmail.com>
2014-09-14 19:05:20 +0200

    Correccion urls

--- kugar/urls.py
+++ kugar/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
+from django.conf import settings
 from django.contrib import admin
 
 urlpatterns = [
@@ -8,3 +9,13 @@ urlpatterns = [
 
     url(r'^', include('kugar_webapp.urls')),
 ]
+
+# Solución a los estáticos en Heroku
+# Mil gracias: http://stackoverflow.com/questions/9047054/heroku-handling-static-files-in-django-app
+# if settings.DEBUG ??
+urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    )
